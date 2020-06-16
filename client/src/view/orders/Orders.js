25c7bd8dc24f2de93b9af53df66e1c3956dd6c71
ben <b_brinberg@hotmail.com>
2020-06-16 16:28:54 +0300

    order table css

--- client/src/view/orders/Orders.js
+++ client/src/view/orders/Orders.js
@@ -12,19 +12,19 @@ class Orders extends React.Component {
         super(props);
         this.state = {
             orders: [],
-            isRefresh:false
+            isRefresh: false
         }
 
 
-    //     this.refresh = this.refresh.bind(this);
+        //     this.refresh = this.refresh.bind(this);
 
 
-    // }
-    //     refresh = (e) => {
-    //     e.preventDefault();
-      
+        // }
+        //     refresh = (e) => {
+        //     e.preventDefault();
+
         let schoolName = this.props.schoolName
-     
+
         fetch('http://localhost:5050/getOrders', {
             method: 'POST',
             body: JSON.stringify(),
@@ -48,7 +48,7 @@ class Orders extends React.Component {
 
                 this.setState({
                     orders: data.results,
-                    isRefresh:true
+                    isRefresh: true
                 })
                 console.log(this.state.orders)
 
@@ -59,9 +59,9 @@ class Orders extends React.Component {
                 console.error(err)
             })
 
-  
+
     }
-    
+
     render() {
         return (
             <div>
@@ -69,30 +69,70 @@ class Orders extends React.Component {
                     return <div>{order}</div>
                 }
                 )} */}
-                
-                {(this.state.isRefresh)?
+
+                {(this.state.isRefresh) ?
 
 
-                <div>  <button onClick={this.refresh}>רענון</button>   
-                <Link to="/orderForm"><button  > עבור לטופס הזמנה  </button></Link>
-                {this.state.orders.map((order,index)=>{
-                    console.log('in map')
-                    console.log(order[index])
-                    if (this.state.orders[index].school_name == this.props.schoolName){
-                    return <div className="setAsRow"> <div> מספר הזמנה {this.state.orders[index].id} ,  </div><div> שם בית ספר {this.state.orders[index].school_name}  ,  </div><div> שם מזמין {this.state.orders[index].orderby_name},</div><div>  
-                     תפקיד המזמין{this.state.orders[index].orderby_position},  </div><div> טלפון המזמין{this.state.orders[index].orderby_phone},  </div><div> מטרת הנסיעה{this.state.orders[index].order_objective},  </div><div>
-                     סוג הזמנה {this.state.orders[index].order_type},  </div><div> תאריך נסיעה{this.state.orders[index].ordertravel_date},  </div><div> יום נסיעה{this.state.orders[index].ordertravel_day},  </div><div>
-                     שעת נסיעה{this.state.orders[index].ordertravel_time},  </div><div> איסוף בלבד:{this.state.orders[index].pickuponly},  </div><div> מספר תלמידים:{this.state.orders[index].order_students_number},  </div><div>
-                     יעד הנסיעה{this.state.orders[index].oredr_destanation},  </div><div> כתובת היעד{this.state.orders[index].order_address},  </div><div> שעת חזרה{this.state.orders[index].order_return_time},  </div><div>
-                     הערות: {this.state.orders[index].order_comments},  </div><div> שם איש קשר:{this.state.orders[index].order_contact_name},  </div><div> תפקיד איש קשר{this.state.orders[index].order_contact_position},  </div><div>
-                     טלפון איש קשר: {this.state.orders[index].order_contact_phone}, מרחק {this.state.orders[index].distance}
-                     , סטטוס {this.state.orders[index].status}</div></div>}
-                }
-                )}</div>
-                
-                
-                : <div><button onClick={this.refresh}>רענון</button>
-                <Link to="/orderForm"><button  > עבור לטופס הזמנה  </button></Link></div>}
+                    <div>  <button onClick={this.refresh}>רענון</button>
+                        <Link to="/orderForm"><button  > עבור לטופס הזמנה  </button></Link>
+                        <div className="orderTable tableHeader">
+                            <div className="border"> מספר הזמנה </div>
+                            <div className="border"> שם בית ספר </div>
+                            <div className="border"> שם מזמין </div>
+                            <div className="border"> תפקיד המזמין</div>
+                            <div className="border"> טלפון המזמין</div>
+                            <div className="border"> מטרת הנסיעה</div>
+                            <div className="border">סוג הזמנה </div>
+                            <div className="border"> תאריך נסיעה</div>
+                            <div className="border"> יום נסיעה</div>
+                            <div className="border"> שעת נסיעה</div>
+                            <div className="border"> איסוף בלבד</div>
+                            <div className="border"> מספר תלמידים</div>
+                            <div className="border"> יעד הנסיעה</div>
+                            <div className="border"> כתובת היעד</div>
+                            <div className="border"> שעת חזרה</div>
+                            <div className="border"> הערות</div>
+                            <div className="border"> שם איש קשר</div>
+                            <div className="border"> תפקיד איש קשר</div>
+                            <div className="border">טלפון איש קשר</div>
+                            <div className="border">מרחק ליעד</div>
+                            <div className="border"> סטטוס הזמנה </div>
+                        </div>
+                        {this.state.orders.map((order, index) => {
+                            console.log('in map')
+                            console.log(order[index])
+                            if (this.state.orders[index].school_name == this.props.schoolName) {
+                                return <div className="orderTable">
+                                    <div className="border">{this.state.orders[index].id}   </div>
+                                    <div className="border">{this.state.orders[index].school_name}    </div>
+                                    <div className="border">{this.state.orders[index].orderby_name}</div>
+                                    <div className="border">{this.state.orders[index].orderby_position}  </div>
+                                    <div className="border">{this.state.orders[index].orderby_phone}</div>
+                                    <div className="border">{this.state.orders[index].order_objective} </div>
+                                    <div className="border">{this.state.orders[index].order_type}  </div>
+                                    <div className="border">{this.state.orders[index].ordertravel_date}  </div>
+                                    <div className="border">{this.state.orders[index].ordertravel_day} </div>
+                                    <div className="border">{this.state.orders[index].ordertravel_time} </div>
+                                    <div className="border">{this.state.orders[index].pickuponly}</div>
+                                    <div className="border">{this.state.orders[index].order_students_number}  </div>
+                                    <div className="border">{this.state.orders[index].order_destanation} </div>
+                                    <div className="border">{this.state.orders[index].order_address}</div>
+                                    <div className="border">{this.state.orders[index].order_return_time}</div>
+                                    <div className="border">{this.state.orders[index].order_comments} </div>
+                                    <div className="border">{this.state.orders[index].order_contact_name}</div>
+                                    <div className="border">{this.state.orders[index].order_contact_position}</div>
+                                    <div className="border">{this.state.orders[index].order_contact_phone}</div>
+                                    <div className="border"> {this.state.orders[index].distance}</div>
+                                    <div className="border">{this.state.orders[index].status}</div>
+                                </div>
+                            }
+                        }
+
+                        )}</div>
+
+
+                    : <div><button onClick={this.refresh}>רענון</button>
+                        <Link to="/orderForm"><button  > עבור לטופס הזמנה  </button></Link></div>}
             </div>
         )
     }
